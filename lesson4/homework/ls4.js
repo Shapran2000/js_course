e6939c4eb9ce1809080a30791f90e7288a1acbf2
Taras Shapran <shaprantaras2000@gmail.com>
2021-07-22 14:34:51 +0300

    done lesson 4 homework

--- lesson4/homework/ls4.js
+++ lesson4/homework/ls4.js
@@ -75,6 +75,7 @@ function mapFunction(numberOfElements){
     let arr = [];
     console.log(`До перетворення :`);
     arr = randArrOfIntNew(numberOfElements);
+    arr.toString();
     let a = arr.map(function (n) {
         return {
             n
@@ -83,3 +84,34 @@ function mapFunction(numberOfElements){
     console.log(a);
 }
 mapFunction(10);
+console.log('- Створити функцію конструктор для об\'єктів User з полями id, name, surname , email, phone\n' +
+    'створити пустий масив, наповнити його 10 об\'єктами new User(....)\n' +
+    'Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об\'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)');
+let user = function (id, name, surname , email, phone){
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+}
+let users = [new user(1,"Leanne","Bret", "Sincere@april.biz","777-777-77-77"),
+    new user(2,"Ervin","Antonette", "Shanna@melissa.tv","777-777-77-77"),
+    new user(3,"Clementine","Samantha", "Nathan@yesenia.net","777-777-77-77"),
+    new user(4,"Patricia","Karianne", "Julianne.OConner@kory.org","777-777-77-77"),
+    new user(5,"Chelsey","Kamren", "Lucio_Hettinger@annie.ca","777-777-77-77"),
+    new user(6,"Dennis","Leopoldo_Corkery", "Karley_Dach@jasper.info","777-777-77-77"),
+    new user(7,"Kurtis","Elwyn", "Telly.Hoeger@billy.biz","777-777-77-77"),
+    new user(8,"Nicholas","Maxime_Nienow", "Sherwood@rosamond.me","777-777-77-77"),
+    new user(9,"Glenna","Delphine", "Chaim_McDermott@dana.i","777-777-77-77"),
+    new user(10,"Clementina","Moriah.Stanton", "Rey.Padberg@karina.biz","777-777-77-77")];
+
+let filterUser= users.filter(function (value) {
+    return value.id%2===0;
+})
+filterUser.sort(function (v1,v2) {
+    return v1.id-v2.id;
+})
+console.log(filterUser);
+
+
+
